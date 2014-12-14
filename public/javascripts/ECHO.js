73ffede1fb1e25ada4862c39d5dc0c86270cd7ff
segensreich <christian@stuebeweg50.de>
2014-12-14 18:00:53 +0100

    Fix: corrected some wrong ; in the jscript of the view

--- public/javascripts/ECHO.js
+++ public/javascripts/ECHO.js
@@ -42,14 +42,14 @@ $(document).ready(function(){
 });
 
 function showAbilities(event){
-	id = event.target.id.split("_")[0]
-	$("#"+id+"_abilities").css({ top: $(event.target).position().top, left: $(event.target).position().left})	
-	$("#"+id+"_abilities").switchClass("discipline_abilities_inv","discipline_abilities_vis",0)
-	};
+	id = event.target.id.split("_")[0];
+	$("#"+id+"_abilities").css({"top":$(event.target).position().top+"px" ,"left": $(event.target).position().left+"px"});	
+	$("#"+id+"_abilities").switchClass("discipline_abilities_inv","discipline_abilities_vis",0);
+	}
 
 function hideAbilities(event){
-	$(".discipline_abilities_vis").switchClass("discipline_abilities_vis","discipline_abilities_inv",0)
-	};
+	$(".discipline_abilities_vis").switchClass("discipline_abilities_vis","discipline_abilities_inv",0);
+	}
 
 
 function add2Matrix(event){
@@ -57,45 +57,45 @@ function add2Matrix(event){
 		url:"/API/characters/"+$("#char_id").text()+"/addmatrix/"+event.target.id+"/",
 		type:"POST"
 		});
-		$(event.target).switchClass("nomatrix","inmatrix")
-	};
+		$(event.target).switchClass("nomatrix","inmatrix");
+	}
 
 function removeFromMatrix(event){
 		$.ajax({
 		url:"/API/characters/"+$("#char_id").text()+"/frommatrix/"+event.target.id+"/",
 		type:"POST"
 		});
-		$(event.target).switchClass("inmatrix","nomatrix")
-	};
+		$(event.target).switchClass("inmatrix","nomatrix");
+	}
 
 function getMatrixPage(event){
 	$("#roller").load("/HTML/matrices/"+$("#char_id").text()+"/"+event.target.id+"/",	
 	function(){	
-	$("#roller").css({ top: $(event.target).position().top, left: $(event.target).position().left})	
-	$("#roller").fadeIn("normal")});
-	};
+	$("#roller").css({ top: $(event.target).position().top, left: $(event.target).position().left});
+	$("#roller").fadeIn("normal");});
+	}
 	
 function weaveThread(event){
-		$("#"+event.target.id+"Result").load("/API/dices/"+$("#"+event.target.id+"_textfield").val()+"/")	
-	};
+		$("#"+event.target.id+"Result").load("/API/dices/"+$("#"+event.target.id+"_textfield").val()+"/");
+	}
 	
 function getSpellPage(event){
 	$("#roller").load("/HTML/spell/"+event.target.id+"/"+$("#char_id").text()+"/",	
 	function(){	
-	$("#roller").css({ top: $(event.target).position().top, left: $(event.target).position().left})	
-	$("#roller").fadeIn("normal")});
-	};
+	$("#roller").css({ top: $(event.target).position().top, left: $(event.target).position().left})	;
+	$("#roller").fadeIn("normal");});
+	}
 
 function throwDice(event){
-		$("#dice_result").load("/API/dices/"+$("#wuerfel_textfield").val()+"/")	
-	};
+		$("#dice_result").load("/API/dices/"+$("#wuerfel_textfield").val()+"/");
+	}
 	
 function getDicePage(event){
 	$("#roller").load("/HTML/dice/"+event.target.className+"/"+event.target.id+"/"+event.target.textContent+"/"+$("#char_id").text()+"/",
 	function(){	
-	$("#roller").css({ top: $(event.target).position().top, left: $(event.target).position().left})	
-	$("#roller").fadeIn("normal")});
-	};
+	$("#roller").css({ top: $(event.target).position().top, left: $(event.target).position().left});
+	$("#roller").fadeIn("normal");});
+	}
 
 function createChar(event){
 		$.ajax({
@@ -103,95 +103,95 @@ function createChar(event){
 		type:"POST",
 		complete:reload_chars
 		});
-	};
+	}
 
 function redirectChar(event){
 		window.location.href = "/char/"+event.target.id+"/";
-	};
+	}
 
 function sandbox(event){
-		$("#char_root").fadeOut("normal")
-	};
+		$("#char_root").fadeOut("normal");
+	}
 
 function threadUp(event){
-	$.get("/API/characters/"+$("#char_id").text()+"/armors/"+event.target.id+"/thread/attach/",reload_char)
-	};
+	$.get("/API/characters/"+$("#char_id").text()+"/armors/"+event.target.id+"/thread/attach/",reload_char);
+	}
 	
 function threadDown(event){
-	$.get("/API/characters/"+$("#char_id").text()+"/armors/"+event.target.id+"/thread/remove/",reload_char)
-	};	
+	$.get("/API/characters/"+$("#char_id").text()+"/armors/"+event.target.id+"/thread/remove/",reload_char);
+	}
 
 function getArmor(event){
-	$.get("/API/characters/"+$("#char_id").text()+"/armors/"+event.target.id+"/get/",reload_char)
-	};
+	$.get("/API/characters/"+$("#char_id").text()+"/armors/"+event.target.id+"/get/",reload_char);
+	}
 	
 function removeArmor(event){
-	$.get("/API/characters/"+$("#char_id").text()+"/armors/"+event.target.id+"/remove/",reload_char)
-	};
+	$.get("/API/characters/"+$("#char_id").text()+"/armors/"+event.target.id+"/remove/",reload_char);
+	}
 	
 function learnSpell(event){
-	$.post("/API/characters/"+$("#char_id").text()+"/spells/"+event.target.id+"/learn/",reload_char)
-	};
+	$.post("/API/characters/"+$("#char_id").text()+"/spells/"+event.target.id+"/learn/",reload_char);
+	}
 
 function unlearnSpell(event){
-	$.post("/API/characters/"+$("#char_id").text()+"/spells/"+event.target.id+"/unlearn/",reload_char)
-	};
+	$.post("/API/characters/"+$("#char_id").text()+"/spells/"+event.target.id+"/unlearn/",reload_char);
+	}
 
 function skillDown(event){	
-	$.post("/API/characters/"+$("#char_id").text()+"/skills/"+event.target.id+"/corrupt/",reload_char)
-	};
+	$.post("/API/characters/"+$("#char_id").text()+"/skills/"+event.target.id+"/corrupt/",reload_char);
+	}
 	
 function skillUp(event){	
-	$.post("/API/characters/"+$("#char_id").text()+"/skills/"+event.target.id+"/improve/",reload_char)
-	};
+	$.post("/API/characters/"+$("#char_id").text()+"/skills/"+event.target.id+"/improve/",reload_char);
+	}
 
 function fetchSkillsBlock(){
-	$("#char_root").fadeOut("normal")
-	$("#float_elem").load("/HTML/skills/")
+	$("#char_root").fadeOut("normal");
+	$("#float_elem").load("/HTML/skills/");
 	window.scrollTo(0, 0);
-	$("#float_elem").fadeIn("normal")	
+	$("#float_elem").fadeIn("normal");
 	}
 
 function fetchSpellsBlock(){
-	$("#char_root").fadeOut("normal")
-	$("#float_elem").load("/HTML/spells/char/"+$("#char_id").text()+"/")
+	$("#char_root").fadeOut("normal");
+	$("#float_elem").load("/HTML/spells/char/"+$("#char_id").text()+"/");
 	window.scrollTo(0, 0);
-	$("#float_elem").fadeIn("normal")	
+	$("#float_elem").fadeIn("normal");	
 	}
 
 function learnDiscipline(event){
-	$.post("/API/characters/"+$("#char_id").text()+"/disciplines/"+event.target.id+"/improve/",reload_char)
-	};
+	$.post("/API/characters/"+$("#char_id").text()+"/disciplines/"+event.target.id+"/improve/",reload_char);
+	}
 	
 function fetchDisciplinesBlock(){
-	$("#char_root").fadeOut("normal")
-	$("#float_elem").load("/HTML/disciplines/")
+	$("#char_root").fadeOut("normal");
+	$("#float_elem").load("/HTML/disciplines/");
 	window.scrollTo(0, 0);
-	$("#float_elem").fadeIn("normal")	
+	$("#float_elem").fadeIn("normal");	
 	}
 	
 function fetchArmorsBlock(){
-	$("#char_root").fadeOut("normal")
-	$("#float_elem").load("/HTML/armors/")
+	$("#char_root").fadeOut("normal");
+	$("#float_elem").load("/HTML/armors/");
 	window.scrollTo(0, 0);
-	$("#float_elem").fadeIn("normal")	
+	$("#float_elem").fadeIn("normal");
 	}
 	
 function disciplineUp(event){	
-	$.post("/API/characters/"+$("#char_id").text()+"/disciplines/"+event.target.id+"/improve/",reload_char)
-	};
+	$.post("/API/characters/"+$("#char_id").text()+"/disciplines/"+event.target.id+"/improve/",reload_char);
+	}
 	
 function disciplineDown(event){	
-	$.post("/API/characters/"+$("#char_id").text()+"/disciplines/"+event.target.id+"/corrupt/",reload_char)
-	};
+	$.post("/API/characters/"+$("#char_id").text()+"/disciplines/"+event.target.id+"/corrupt/",reload_char);
+	}
 
 function fetchRaceBlock(){
-	$("#race").load("/HTML/races/")
+	$("#race").load("/HTML/races/");
 	}
 
 function changeRace(event){
-	$.post("/API/characters/"+$("#char_id").text()+"/race/"+event.target.id+"/",reload_char)
-	};
+	$.post("/API/characters/"+$("#char_id").text()+"/race/"+event.target.id+"/",reload_char);
+	}
 	
 
 function changeCharName(){
@@ -201,51 +201,50 @@ function changeCharName(){
 		data:JSON.stringify({name:$("#name").val()}),
 		dataType:"json",
 		contentType: "application/json"
-		})
-	};
+		});
+	}
 	
 function talentUp(event){
 	
-	$.post("/API/characters/"+$("#char_id").text()+"/talents/"+event.target.id+"/improve/",reload_char)
-	};
+	$.post("/API/characters/"+$("#char_id").text()+"/talents/"+event.target.id+"/improve/",reload_char);
+	}
 	
 function talentDown(event){
 	
-	$.post("/API/characters/"+$("#char_id").text()+"/talents/"+event.target.id+"/corrupt/",reload_char)
-	};
+	$.post("/API/characters/"+$("#char_id").text()+"/talents/"+event.target.id+"/corrupt/",reload_char);
+	}
 	
 function attributeUp(event){
 	
-	$.post("/API/characters/"+$("#char_id").text()+"/attributes/"+event.target.id+"/improve/",reload_char)
-	};
+	$.post("/API/characters/"+$("#char_id").text()+"/attributes/"+event.target.id+"/improve/",reload_char);
+	}
 	
-function attributeDown(event){
-	
-	$.post("/API/characters/"+$("#char_id").text()+"/attributes/"+event.target.id+"/corrupt/",reload_char)
-	};
+function attributeDown(event){	
+	$.post("/API/characters/"+$("#char_id").text()+"/attributes/"+event.target.id+"/corrupt/",reload_char);
+	}
 
 function attributePPUp(event){
-	$.post("/API/characters/"+$("#char_id").text()+"/attributes/"+event.target.id+"/improve/pp/",reload_char)
-	};
+	$.post("/API/characters/"+$("#char_id").text()+"/attributes/"+event.target.id+"/improve/pp/",reload_char);
+	}
 	
 function attributePPDown(event){
-	$.post("/API/characters/"+$("#char_id").text()+"/attributes/"+event.target.id+"/corrupt/pp/",reload_char)
-	};
+	$.post("/API/characters/"+$("#char_id").text()+"/attributes/"+event.target.id+"/corrupt/pp/",reload_char);
+	}
 
 function reload_talents(event){	
-	$("#talents").load("/HTML/char/"+$("#char_id").text()+"1/talents/")
-	};
+	$("#talents").load("/HTML/char/"+$("#char_id").text()+"1/talents/");
+	}
 	
 function reload_char(event){	
-	$("#char_root").fadeIn("normal")
-	$("#float_elem").fadeOut("normal")
-	$("#roller").fadeOut("normal")
-	$("#char_root").load("/HTML/char/"+$("#char_id").text()+"/")
-	$("#float_elem").empty()
-	};
+	$("#char_root").fadeIn("normal");
+	$("#float_elem").fadeOut("normal");
+	$("#roller").fadeOut("normal");
+	$("#char_root").load("/HTML/char/"+$("#char_id").text()+"/");
+	$("#float_elem").empty();
+	}
 	
 function reload_chars(event){	
-	$("#chars_root").fadeIn("normal")
-	$("#float_elem").fadeOut("normal")
-	$("#chars_root").load("/HTML/chars/")	
-	};
+	$("#chars_root").fadeIn("normal");
+	$("#float_elem").fadeOut("normal");
+	$("#chars_root").load("/HTML/chars/");
+	}
