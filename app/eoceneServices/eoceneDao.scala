25be57e3e149ca15c74bee184ae031c6e44545d0
cgars <christian@stuebeweg50.de>
2016-01-25 12:34:27 +0100

    Imp: Naming in the DAO Trait

--- app/eoceneServices/eoceneDao.scala
+++ app/eoceneServices/eoceneDao.scala
@@ -1,10 +1,15 @@
+/*
+ * Copyright (c) 2016 Christian Garbers.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Simplified BSD License
+ *  which accompanies this distribution
+ *  Contributors:
+ *       Christian Garbers - initial API and implementation
+ */
+
 package eoceneServices
 
-import models.Race
-import models.Discipline
-import models.Spell
-import models.Skill
-import models.Character
+import models.{Character, Discipline, Race, Skill}
 
 trait eoceneDao {
   def createCharByName(name: String)(implicit user: eoceneServices.EoceneUser): Option[Int]
@@ -15,36 +20,58 @@ trait eoceneDao {
   def getRaceById(id: Int): Option[Race]
   def getCharById(id: Int): Option[Character]
   def getCharIdByName(name: String): Option[Int]
-  def changeCharRace(id: Int, id_race: Int): Boolean
+
+  def changeCharRace(id: Int, idRace: Int): Boolean
   def getRaceIdByCharId(id: Int): Option[Int]
   def getCharAttribute(id: Int, attribute: String): Option[Int]
-  def updateCharAttribute(id: Int, attribute: String, new_value: Int): Boolean
+
+  def updateCharAttribute(id: Int, attribute: String, newValue: Int): Boolean
   def updateCharAttributeWithPP(id: Int, attribute: String, direction: String): Boolean
   def updateCharAttributeWithLP(id: Int, attribute: String, direction: String): Boolean
   def getCharDisciplineRowsByCharId(id: Int): List[List[Int]]
-  def improveCharDiscipline(id: Int, id_discipline: Int): Boolean
-  def corruptCharDiscipline(id: Int, id_discipline: Int): Boolean
+
+  def improveCharDiscipline(id: Int, idDiscipline: Int): Boolean
+
+  def corruptCharDiscipline(id: Int, idDiscipline: Int): Boolean
   def getCharTalentRowsIdByCharId(id: Int): List[List[Int]]
-  def getTalentCircleByTalenAndDisciId(id_talent: Int, id_char: Int): Int
-  def improveCharTalent(id: Int, id_talent: Int): Boolean
-  def corruptCharTalent(id: Int, id_talent: Int): Boolean
+
+  def getTalentCircleByTalenAndDisciId(idTalent: Int, idChar: Int): Int
+
+  def improveCharTalent(id: Int, idTalent: Int): Boolean
+
+  def corruptCharTalent(id: Int, idTalent: Int): Boolean
   def getCharSkillRowsByCharId(id: Int): List[List[Int]]
-  def improveCharSkill(id: Int, id_skill: Int): Boolean
-  def corruptCharSkill(id: Int, id_skill: Int): Boolean
+
+  def improveCharSkill(id: Int, idSkill: Int): Boolean
+
+  def corruptCharSkill(id: Int, idSkill: Int): Boolean
   def getCharSpellListByCharId(id: Int): List[List[Int]]
-  def learnCharSpell(id: Int, id_Spell: Int): Boolean
-  def unlearnCharSpell(id: Int, id_Spell: Int): Boolean
+
+  def learnCharSpell(id: Int, idSpell: Int): Boolean
+
+  def unlearnCharSpell(id: Int, idSpell: Int): Boolean
   def changeCharName(id: Int, name: String): Boolean
-  def getArmor(id: Int, id_armor: Int): Boolean
-  def removeArmor(id: Int, id_armor: Int): Boolean
-  def attachThreadArmor(id: Int, id_armor: Int): Boolean
-  def removeThreadArmor(id: Int, id_armor: Int): Boolean
-  def Spell2Matrix(id_spell: Int, id_char: Int): Boolean
-  def SpellFromMatrix(id_spell: Int, id_char: Int): Boolean
-  def removeUserFromChar(id_char: Int, id_user: String): Boolean
-  def shareChar(id_char: Int, user_mail: String): Boolean
-  def buyKarma(id_char: Int, nr_points: Int): Boolean
-  def spentKarma(id_char: Int, nr_points: Int): Boolean
-  def addLP(id_char: Int, nr_points: Int): Boolean
+
+  def getArmor(id: Int, idArmor: Int): Boolean
+
+  def removeArmor(id: Int, idArmor: Int): Boolean
+
+  def attachThreadArmor(id: Int, idArmor: Int): Boolean
+
+  def removeThreadArmor(id: Int, idArmor: Int): Boolean
+
+  def Spell2Matrix(idSpell: Int, idChar: Int): Boolean
+
+  def SpellFromMatrix(idSpell: Int, idChar: Int): Boolean
+
+  def removeUserFromChar(idChar: Int, idUser: String): Boolean
+
+  def shareChar(idChar: Int, userMail: String): Boolean
+
+  def buyKarma(idChar: Int, nrPoints: Int): Boolean
+
+  def spentKarma(idChar: Int, nrPoints: Int): Boolean
+
+  def addLP(idChar: Int, nrPoints: Int): Boolean
   def storeAction(call: String, id_char: Int, id_user: String): Boolean
 }
